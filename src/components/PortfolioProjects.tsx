1e2dabf51d8e4a211ea894c73237137a34774c47
jaydeepux <jaydeepux@gmail.com>
2026-05-27 03:07:28 +0530

    Update PortfolioProjects.tsx

--- src/components/PortfolioProjects.tsx
+++ src/components/PortfolioProjects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import Image from "next/image";
 import * as THREE from "three";
 import { motion, useScroll, useTransform } from "framer-motion";
 
